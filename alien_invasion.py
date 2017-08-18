9ee15f6b2365efef98457eff32ab666b1f1e5d63
arron <2376827533@qq.com>
2017-08-18 22:31:46 +0800

    alien_invasion
    2017.8.18

---
+++ alien_invasion.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+def run_game():
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+	ship = Ship(ai_settings,screen)
+	bullets = Group()
+	aliens = Group()
+	gf.create_fleet(ai_settings,screen,ship,aliens)
+
+	while True:
+		gf.check_events(ai_settings,screen,ship,bullets)
+		gf.update_ship(ship)
+		gf.update_bullets(bullets)
+		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+run_game()
